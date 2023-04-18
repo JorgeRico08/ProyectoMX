6cb8a442f566dddfef2f2237950aafbdb09e7b91
Araiza07 <jezziaraiza@gmail.com>
2023-04-18 15:36:58 -0600

    Agregando cambios de lenguaje

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,6 +1,6 @@
 const isAdmin=(req,res,next)=>{
 if( req.user.role!=="Admin"){
-    req.flash("error","Necesita ser Admin para continuar..")
+    req.flash("error","Necesitas estar registrado para continuar..")
     console.log("\n\n\n\n\ Esta es la url de Admin \n\n\n\n")
     console.log(req.session.previousUrl)
     res.redirect(req.session.previousUrl)
